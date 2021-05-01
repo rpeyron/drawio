9b85d670f2a4a9e1678638f29b058d9798221449
Remi Peyronnet <remi+github@via.ecp.fr>
2021-05-01 21:32:07 +0200

    Update PreConfig.js

--- src/main/webapp/js/PreConfig.js
+++ src/main/webapp/js/PreConfig.js
@@ -9,4 +9,5 @@ window.DRAWIO_BASE_URL = null; // Replace with path to base of deployment, e.g. 
 window.DRAWIO_VIEWER_URL = null; // Replace your path to the viewer js, e.g. https://www.example.com/js/viewer.min.js
 window.DRAW_MATH_URL = 'math';
 window.DRAWIO_CONFIG = null; // Replace with your custom draw.io configurations. For more details, https://www.diagrams.net/doc/faq/configure-diagram-editor
+window.DRAWIO_CONFIG = {"plugins": ["/plugins/editors-plugin.webpack.js"], "splash": 0} 
 urlParams['sync'] = 'manual';
